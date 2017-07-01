e25be84534c72a158b721ff12b904076d65da2e9
Unknown <071bct538@pcampus.edu.np>
2017-07-01 18:18:04 +0545

    fixing date issue!

--- music/models.py
+++ music/models.py
@@ -20,7 +20,7 @@ class Album(models.Model):
     This table contains Album, with Name and Year
     """
     Album_title = models.CharField(max_length=500)
-    album_year = models.DateTimeField()
+    album_year = models.CharField(max_length=20)
 
     def __str__(self):
         return self.Album_title
@@ -60,7 +60,7 @@ class Playlist(models.Model):
     """
     It contains a different songs that should be in a given Playlist
     """
-    song_id = models.ManyToManyField(Song, on_delete=models.CASCADE)
+    song_id = models.ManyToManyField(Song)
 
 class UserPlaylist(models.Model):
     """
@@ -69,11 +69,11 @@ class UserPlaylist(models.Model):
     playlist_id = models.OneToOneField(Playlist, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Genre(models.model):
+class Genre(models.Model):
     """
     A list of Genre for a given song.
     """
-    name = models.charfield(max_length=500)
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
@@ -82,7 +82,7 @@ class SongAlbum(models.Model):
     """
     This contains list of songs for a given album.
     """
-    song_id = models.ManyToManyField(Song, on_delete=models.CASCADE)
+    song_id = models.ManyToManyField(Song)
     album_id = models.OneToOneField(Album, on_delete=models.CASCADE)
 
 class SongRating(models.Model):
@@ -90,54 +90,54 @@ class SongRating(models.Model):
     It is a table for containing the rating for given song from multiple users.
     """
     rating = models.CharField(max_length=5)
-    user_id = models.ManyToManyField(User, on_delete=models.CASCADE)
+    user_id = models.ManyToManyField(User)
     song_id = models.OneToOneField(Song, on_delete=models.CASCADE)
 
 class SongArtist(models.Model):
     """
     Relates the number of songs for a given Artist
     """
-    song_id = models.ManyToManyField(Song, on_delete=models.CASCADE)
+    song_id = models.ManyToManyField(Song)
     artist_id = models.OneToOneField(Artist, on_delete=models.CASCADE)
 
 class SongGenre(models.Model):
     """
     links to the Genre for given song
     """
-    genre_id = models.ManyToManyField(Genre, on_delete=models.CASCADE)
+    genre_id = models.ManyToManyField(Genre)
     song_id = models.OneToOneField(Song, on_delete=models.CASCADE)
 
-class ArtistKeyword(models.model):
+class ArtistKeyword(models.Model):
     """
     Identifying Artist using keyword
     """
-    keyword_id = models.ManyToManyField(Keyword, on_delete=models.CASCADE)
+    keyword_id = models.ManyToManyField(Keyword)
     artist_id = models.OneToOneField(Artist, on_delete=models.CASCADE)
 
-class SongKeyword(models.model):
+class SongKeyword(models.Model):
     """
     Identifying the song from given keyword
     """
-    keyword_id = models.ManyToManyField(Keyword, on_delete=models.CASCADE)
+    keyword_id = models.ManyToManyField(Keyword)
     song_id = models.OneToOneField(Song, on_delete=models.CASCADE)
 
-class AlbumKeyword(models.model):
+class AlbumKeyword(models.Model):
     """
     Identifying albums from keyword
     """
     album_id = models.OneToOneField(Album, on_delete=models.CASCADE)
-    keyword_id = models.ManyToManyField(Keyword, on_delete=models.CASCADE)
+    keyword_id = models.ManyToManyField(Keyword)
 
-class PlaylistKeyword(models.model):
+class PlaylistKeyword(models.Model):
     """
     Identifying Playlist from keyword
     """
     playslist_id = models.OneToOneField(Playlist, on_delete=models.CASCADE)
-    keyword_id = models.ManyToManyField(Keyword, on_delete=models.CASCADE)
+    keyword_id = models.ManyToManyField(Keyword)
 
-class RatingKeyword(models.model):
+class RatingKeyword(models.Model):
     """
     Identifying Playlist from keyword
     """
     rating_id = models.OneToOneField(SongRating, on_delete=models.CASCADE)
-    keyword_id = models.ManyToManyField(Keyword, on_delete=models.CASCADE)
+    keyword_id = models.ManyToManyField(Keyword)
