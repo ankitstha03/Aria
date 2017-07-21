1050e9343b36755962c365d6e56fcd4a9eb6d2d8
ankitstha3 <ankitstha3@gmail.com>
2017-07-21 22:11:43 +0545

    Add files via upload

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,8 +3,13 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = User
-        fields = ['first_name','username', 'email', 'password']
+	first_name= forms.CharField(widget=forms.TextInput(attrs={'placeholder':'First Name'}))
+	last_name= forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Last Name'}))
+	username= forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
+	email= forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'E-mail'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+	confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}))
+    
+	class Meta:
+		model = User
+		fields = ['first_name', 'last_name','username', 'email', 'password']
