3fd69be0265a48904a388ec04efc93faf0ee2650
Suyog <suyogwaitforitsng@gmail.com>
2017-07-26 08:04:54 +0545

    playlists can play songs

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 
     url(r'^playlist/$', views.PlaylistView.as_view(), name='playlist'),
     url(r'playlist/add/$', views.PlaylistAddView.as_view(), name='playlist_add'),
+    url(r'playlist/(?P<pk>[0-9]+)/$', views.PlaylistUpdate.as_view(), name='playlist_update'),
+    url(r'playlist/delete/(?P<pk>[0-9]+)/$', views.PlaylistDelete.as_view(), name='playlist_delete'),
+    url(r'playlist/(?P<playlist_id>[0-9]+)/play/$', views.PlaylistPlay, name='playlist_play'),
     #url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name='favorite'),
     #url(r'^songs/$', views.SongList.as_view(), name='song_list'),
 ]
--- music/views.py
+++ music/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View, ListView, CreateView, DeleteView, UpdateV
 from .models import Album, Song, Playlist
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
 from .forms import *
 from utils.views import *
 # Create your views here
@@ -53,3 +54,9 @@ class PlaylistUpdate(UpdateView):
     model = Playlist
     fields = '__all__'
     success_url = reverse_lazy('PlaylistList')
+
+def PlaylistPlay(request,playlist_id):
+    playlist = get_object_or_404(Playlist,pk=playlist_id)
+    return render_to_response('music/playlist_play.html',{
+    "playlist" : playlist
+    })
